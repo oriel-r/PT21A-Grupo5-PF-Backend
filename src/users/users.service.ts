ab74c0a483cf54e84f48d9e20684a8e710d70d2f
Rocio Bonin <rociobonindev@gmail.com>
2024-11-06 21:51:44 -0300

    Pagination

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -16,6 +16,16 @@ export class UsersService {
     private readonly usersRepo: UsersRepository,
   ) {}
 
+  async pagination(page: number, limit: number) {
+    const offset = (page - 1) * limit;
+
+    return await this.usersRepository.find({
+        select: ['id', 'idNumber', 'email', 'courses', 'createdAt', 'name', 'role', 'subscription'],
+        skip: offset,
+        take: limit,
+    });
+}
+
   async create(createUserDto: CreateUserDto) {
     const { name, email, idNumber } = createUserDto;
     const hashedPassword = await hash(idNumber, 10);
