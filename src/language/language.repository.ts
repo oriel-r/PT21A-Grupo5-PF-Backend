0d59bc8ad3050196b9c4a6d537ff31c13063cdb2
oriel <orielromero97@gmail.com>
2024-11-09 13:57:40 -0300

    Language crud work in progress

---
+++ src/language/language.repository.ts
@@ -0,0 +1,35 @@
+import { InjectRepository } from "@nestjs/typeorm";
+import { Language } from "./entities/language.entity";
+import { DeleteResult, Repository, UpdateResult } from "typeorm";
+
+export class LanguageRepository {
+    constructor(
+        @InjectRepository(Language) private readonly languageRepository: Repository<Language>
+    ) {}
+
+    async getAll() {
+        return await this.languageRepository.find()
+    }
+
+    async findById(id: string): Promise<Language | null> {
+        return await this.languageRepository.findOneBy({id})
+    }
+
+    async findByName(name:string): Promise <Language | null> {
+        return await this.languageRepository.findOneBy({name})
+    }
+
+    async create(data): Promise<Language> {
+        return await this.languageRepository.save(
+            this.languageRepository.create(data)
+        )[0]
+    }
+
+    async update(id, data): Promise<UpdateResult> {
+        return await this.languageRepository.update(id, data)
+    }
+
+    async delete(id:string): Promise<DeleteResult> {
+        return await this.languageRepository.delete(id)
+    }
+}
--- src/language/language.service.ts
+++ src/language/language.service.ts
@@ -1,4 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, ServiceUnavailableException } from '@nestjs/common';
+import { LanguageRepository } from './language.repository';
+import { Language } from './entities/language.entity';
+import { CreateLanguageDto } from './dto/create-language.dto';
+import { CloudinaryService } from 'src/services/cloudinary/cloudinary.service';
 
 @Injectable()
-export class LanguageService {}
+export class LanguageService {
+    constructor(
+        private readonly languageRepository: LanguageRepository,
+        private readonly fileUploadService: CloudinaryService
+    ) {}
+
+    async getAll() {
+        const languages = await this.languageRepository.getAll()
+        if(!languages) throw new NotFoundException('Languages not found')
+        return languages
+    }
+
+    async getById(id: string): Promise<Language> {
+        const language = await this.languageRepository.findById(id)
+        if(!language) throw new NotFoundException('Language not found')
+        return language
+    }
+
+    async getByName(name: string): Promise<Language> {
+        const language = await this.languageRepository.findByName(name)
+        if(!language) throw new NotFoundException('Language not found')
+        return language
+    }
+
+    async addLanguage(data: CreateLanguageDto, file): Promise<Language | null> {
+        const url = await this.fileUploadService.uploadFile(file)
+        if(!url) throw new ServiceUnavailableException('Have a problem from external service, retry later')
+        data.image_url = url
+        const newLanguage = await this.languageRepository.create(data)
+        return newLanguage
+    } 
+}
