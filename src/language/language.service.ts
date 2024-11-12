20ddf526b795a67df32f28f5ea77c41a22ac09ba
oriel <orielromero97@gmail.com>
2024-11-12 17:09:13 -0300

    Se agrego manejo de errores

--- src/language/language.service.ts
+++ src/language/language.service.ts
@@ -13,6 +13,8 @@ import { CloudinaryService } from 'src/services/cloudinary/cloudinary.service';
 import { DeepPartial } from 'typeorm';
 import { Course } from 'src/courses/entities/course.entity';
 import { FilterCourses } from 'src/helpers/Filter';
+import { Level } from 'src/enums/level.enum';
+import { Specialization } from 'src/enums/specializations.enum';
 
 @Injectable()
 export class LanguageService {
@@ -30,6 +32,8 @@ export class LanguageService {
   }
 
   async getAndFilter(path: string, filter: FilterCourses) {
+    if (filter.level && !Object.values(Level).includes(filter.level)) throw new BadRequestException('El nivel buscado no existe')
+    if (filter.specialization && !Object.values(Specialization).includes(filter.specialization)) throw new BadRequestException('la especialización buscado no existe')
     const result = await this.languageRepository.getAndFilter(path, filter)
     if(!result) throw new NotFoundException('No se encontraron resultados')
     return result
