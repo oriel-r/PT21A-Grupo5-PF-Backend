626833c98108acfd65e23a9bf1fd7e68176618f1
matiast86 <130871628+matiast86@users.noreply.github.com>
2024-11-15 00:12:30 -0300

    Logger

--- src/middlewares/loger/loger.middleware.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
-
-@Injectable()
-export class LogerMiddleware implements NestMiddleware {
-  use(req: any, res: any, next: () => void) {
-    console.log(`A ${req.method} in ${req.url} at ${new Date()}`)
-    next();
-  }
-}
