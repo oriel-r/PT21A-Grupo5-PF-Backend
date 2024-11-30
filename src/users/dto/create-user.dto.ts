5d0b7542b4c1d0c95006bd1b0b40eafc80315d08
matiast86 <130871628+matiast86@users.noreply.github.com>
2024-11-30 07:08:49 -0300

    teacherDto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -21,15 +21,7 @@ export class CreateUserDto {
     description: 'The password of the user',
     example: 'Password123!',
   })
-  @Matches(
-    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[=!@#$%^&*])[A-Za-z\d=!@#$%^&*]{8,15}$/,
-    {
-      message:
-        'La contraseña debe contener al menos una minúscula, una mayúscula, un número, un caracter especial (= !@#$%^&*) y tener entre 8 y 15 caracteres',
-    },
-  )
-  @IsString()
-  @IsNotEmpty()
+  @IsOptional()
   password: string;
 
   @ApiProperty({ enum: Role, description: 'Enum indicating the user role.' })
